fea44d98a0b5899a84948d1e7ddf1399ba0d2f76
Ryno <rjrfourie@outlook.com>
2021-07-02 22:44:23 +0200

    Update class to className

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -2,37 +2,37 @@ import React from "react";
 
 function TouristInfoCards() {
   return (
-    <div class="card-group">
-      <div class="card">
+    <div className="card-group">
+      <div className="card">
         <img
           src="https://peoplemakeglasgow.com/images/Things_to_do/Top_attractions/Main-image-995.JPG"
-          class="card-img-top"
+          className="card-img-top"
           alt="Glasgow Tower"
         />
-        <div class="card-body">
-          <h5 class="card-title">Glasgow Visitor Information</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Glasgow Visitor Information</h5>
+          <p className="card-text">
             Glasgow Life delivers services on behalf of Glasgow City Council to
             both citizens and visitors to the city, providing opportunities
             through culture, sport and learning.
           </p>
           <a
             href="https://peoplemakeglasgow.com/images/Things_to_do/Top_attractions/Main-image-995.JPG"
-            class="btn btn-primary"
+            className="btn btn-primary"
           >
             More information
           </a>
         </div>
       </div>
-      <div class="card">
+      <div className="card">
         <img
           src="https://eu-assets.simpleview-europe.com/manchester2016/imageresizer/?image=%2Fdmsimgs%2FVisit_Manchester_-_new_train_image_Marketing_-_1220x619_-_Fleet_1450125440.jpg&action=ProductDetailFullWidth2"
-          class="card-img-top"
+          className="card-img-top"
           alt="TransPennie Express"
         />
-        <div class="card-body">
-          <h5 class="card-title">Manchester Visitor Information</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Manchester Visitor Information</h5>
+          <p className="card-text">
             Whatever brings you to Manchester, Visit Manchester is here to make
             sure that you get the most out of your trip. When you’re in the
             city, make sure to drop by our Visitor Information Centre. Here you
@@ -40,20 +40,20 @@ function TouristInfoCards() {
             as maps, merchandise and discounted tickets to some of the most
             popular attractions and tourist spots.{" "}
           </p>
-          <a href="visitmanchester.com" class="btn btn-primary">
+          <a href="https://visitmanchester.com" className="btn btn-primary">
             More information
           </a>
         </div>
       </div>
-      <div class="card">
+      <div className="card">
         <img
           src="https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/london-attractions/kew-gardens/kew-gardens-bridge.jpg?mw=640&hash=18C6FCF8FA6D06AE21D27B34E9F2BACB167B60B1"
-          class="card-img-top"
+          className="card-img-top"
           alt="101 things to do outdoors in London"
         />
-        <div class="card-body">
-          <h5 class="card-title">London Visitor Information</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">London Visitor Information</h5>
+          <p className="card-text">
             Welcome to London! Discover the best of London with Visit London,
             the official guide to England’s exciting capital. Find things to do
             in London, from iconic sightseeing spots and fun-filled days out to
@@ -61,7 +61,7 @@ function TouristInfoCards() {
             able to visit just yet, plan ahead to make the most of your next
             visit.
           </p>
-          <a href="visitlondon.com" class="btn btn-primary">
+          <a href="https://visitlondon.com" className="btn btn-primary">
             More information
           </a>
         </div>
